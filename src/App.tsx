66de23d8124fbb2b774fe2408c14adb37bf70cf1
alexkwitko <60580165+alexkwitko@users.noreply.github.com>
2025-04-18 14:20:51 -0500

    Copy src/App.tsx from MBJJWebNew

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import Layout from './components/layout/Layout';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ClassesPage from './pages/ClassesPage';
+import SchedulePage from './pages/SchedulePage';
+import InstructorsPage from './pages/InstructorsPage';
+import ShopPage from './pages/ShopPage';
+import ProductPage from './pages/ProductPage';
+import CartPage from './pages/CartPage';
+import ContactPage from './pages/ContactPage';
+import { CartProvider } from './contexts/CartContext';
+import ChatWidget from './components/ChatWidget';
+
+function App() {
+  return (
+    <HelmetProvider>
+      <CartProvider>
+        <Router>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/classes" element={<ClassesPage />} />
+              <Route path="/schedule" element={<SchedulePage />} />
+              <Route path="/instructors" element={<InstructorsPage />} />
+              <Route path="/shop" element={<ShopPage />} />
+              <Route path="/shop/:productId" element={<ProductPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+            </Routes>
+            <ChatWidget />
+          </Layout>
+        </Router>
+      </CartProvider>
+    </HelmetProvider>
+  );
+}
+
+export default App;
